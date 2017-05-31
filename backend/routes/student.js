9f11d6e2cb749defafb90ba25fcae7892332d7ba
27678 <490216135@qq.com>
2017-05-31 18:27:52 +0800

    修改 学生访问首页

--- backend/routes/student.js
+++ backend/routes/student.js
@@ -1,42 +1,16 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
-var config = require('../model/config');
-var connection = mysql.createConnection(config.mysql);
+var student = require('../model/student');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    connection.connect();
-    var sql = 'SELECT * FROM `student` WHERE 1 ORDER BY id DESC LIMIT ? , ?';
-    var param = [
-        0,
-        10
-    ];
-    connection.query(
-        sql,
-        param,
-        function (err, result) {
-            if(result.length >= 1){
-                res.render(
-                    'student/index',
-                    {
-                        title: '学生管理',
-                        href: '/student',
-                        num: result.length,
-                        data: result
-                    }
-                );
-            }
-        }
-    );
-    connection.end();
+    student.selectPage(req, res, next);
 });
 
 router.get('/add', function(req, res, next) {
     res.render('student/add', { title: '学生添加' });
 });
 router.post('/add', function(req, res, next) {
-    connection.connect();
     var sql = 'INSERT INTO `student`.`student`(`name`, `birth`, `sex`, `description`) VALUES (?, ?, ?, ?)';
     var param = [
         req.body.name,
@@ -44,6 +18,7 @@ router.post('/add', function(req, res, next) {
         req.body.sex,
         req.body.description
     ];
+    connection.connect();
     connection.query(
         sql,
         param,
